ab8593766e3f8c99f654a67becff1cf66ae69cf2
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-09-08 22:45:51 +0200

    initial commit

---
+++ resources/assets/js/front-end/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+          <h1 className="display-3">hello App</h1>
+        );
+    }
+}
+
+export default App;
