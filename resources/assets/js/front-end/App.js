045006a61ae71ba4b4a389b414e4d7fdc418b8f0
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-09-09 22:29:05 +0200

    add react-router package

--- resources/assets/js/front-end/App.js
+++ resources/assets/js/front-end/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Home from './components/Home';
+import Categories from './components/Categories';
+import About from './components/About';
+import Navbar from './components/Navbar';
 
 class App extends Component {
     render() {
         return (
-          <h1 className="display-3">hello App</h1>
+          <div>
+            <Navbar />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/categories' component={Categories} />
+            <Route exact path='/about' component={About} />            
+          </div>
         );
     }
 }
