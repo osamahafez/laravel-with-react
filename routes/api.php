224725f647519119a7580a53dd2c94894324a202
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-09-11 01:24:42 +0200

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/categories/all', 'api\CategoryController@index'); // actual route is: api/categories/all
+Route::post('/categories/store', 'api\CategoryController@store'); // actual route is: api/categories/store
